fe1b6676ec4bc1dd99685f7efe5ae4e9b17d9ec9
mgcali <mgcali@my.waketech.edu>
2022-04-27 16:31:09 -0400

    mp4 to svg mobile changes

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import BarGraph from '../Media/Languages.mp4';
-import Diagram from '../Media/techknowledge.mp4';
+import BarGraph from '../Media/Languages.svg';
+import Diagram from '../Media/techknowledge.svg';
 import { MDBContainer } from "mdbreact";
 
 
@@ -9,12 +9,11 @@ export default function Skills() {
     <MDBContainer className='text-white text-center p-5 mt-5 '>
       <h1> Skills </h1>
       <p>Here are some of my current languages and technical knowledge. Like technology, my skills are always evolving to keep up with the latest technologies.</p>
-       <video autoPlay muted >
-            <source src={BarGraph} type="video/mp4" />
-            </video>
-            <video autoPlay muted >
-            <source src={Diagram} type="video/mp4" />
-            </video>
+       
+            <img src={BarGraph} alt="language bar graph"/>
+            <br/> <br/>
+            <img src={Diagram} alt="technical knowledge chart"/>
+            
     
     </MDBContainer>
   )

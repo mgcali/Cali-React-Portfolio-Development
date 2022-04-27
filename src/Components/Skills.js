2c337060b0d2e68ab040f67517a3f1d450347ec2
mgcali <mgcali@my.waketech.edu>
2022-04-27 17:44:56 -0400

    update text and added img responsiveness
    added responsive containers for skills and blog imgs

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import BarGraph from '../Media/Languages.svg';
 import Diagram from '../Media/techknowledge.svg';
 import { MDBContainer } from "mdbreact";
@@ -7,6 +9,7 @@ import { MDBContainer } from "mdbreact";
 export default function Skills() {
   return (
     <MDBContainer className='text-white text-center p-5 mt-5 '>
+     <div className='container'> 
       <h1> Skills </h1>
       <p>Here are some of my current languages and technical knowledge. Like technology, my skills are always evolving to keep up with the latest technologies.</p>
        
@@ -14,7 +17,7 @@ export default function Skills() {
             <br/> <br/>
             <img src={Diagram} alt="technical knowledge chart"/>
             
-    
+            </div>
     </MDBContainer>
   )
 }
